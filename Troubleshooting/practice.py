0b8b0d2eefc0259c5917e19e1d7ab6fc70569c77
JEONGWON <jaemoongie0@gmail.com>
2026-09-25 03:03:56 +0900

    hw_TIL_2026-09-25

--- Troubleshooting/practice.py
+++ Troubleshooting/practice.py
@@ -1,45 +1,36 @@
-# SWEA 3124. 최소 스패닝 트리
+# SWEA 1952. 수영장
 
-# 1차 시도: 20:37
+# 복습: PASS(18분)
 
-# 목표: 
-def find(x):    # union의 대표를 반환함
-    if parent[x] != x:
-        parent[x] = find(parent[x])
-    return parent[x]
+# 목표: 1년치 수영장 사용 일수가 달 별로 주어질 때, 적절한 요금제를 사용해서 최저 요금을 구하여라
+# 상태: cost = 현재까지의 누적 요금, month = 현재 개월 수
+# 자료구조: DFS
+# 핵심 로직: 
+    # 1. 사용 가능한 1개월, 3개월 요금의 조합을 모두 탐색해서 최저 요금을 구한 후 Y와 비교
+    # 2. 1개월 요금의 경우 D * list(month), M과 비교해서 최소값을 선택
+# 종료 조건: 현재 누적 비용 > 현재 탐색 최솟값이면 return, month >= 12 일 경우 Y와 cost 비교 후 종료
 
-def union(a, b):    # b를 a union으로 포섭함
-    ra = find(a)
-    rb = find(b)
+def dfs(month, cost):
+    global result
 
-    if ra != rb:
-        parent[rb] = ra
+    if cost >= result:
+        return
+
+    if month >= 12:
+        result = min(result, cost)
+        return
+
+    dfs(month + 1, cost + min(plan[month] * D, M))
+
+    dfs(month + 3, cost + M3)
 
 T = int(input())
 for tc in range(1, T + 1):
-    V, E = map(int, input().split())
+    D, M, M3, Y = map(int, input().split())
+    plan = list(map(int, input().split()))
 
-    parent = [i for i in range(V + 1)]  # 첫 시작은 각 정점의 대표자가 자기 자신!
-    edges = []  # 분류하기 쉽게 만드려고 만든 리스트
+    result = Y
 
-    for _ in range(E):
-        a, b, cost = map(int, input().split())
-        edges.append((cost, a, b))
+    dfs(0, 0)
 
-    edges.sort()    # 비용 기준 오름차순으로 정렬
-
-    total = 0   # 총 비용?
-    count = 0   # 선택한 간선의 개수
-
-    for cost, a, b in edges:
-
-        if find(a) != find(b):
-            union(a, b)
-
-            total += cost
-            count += 1
-
-            if count == V - 1:
-                break
-
-    print(f'#{tc} {total}')
+    print(f'#{tc} {result}')
